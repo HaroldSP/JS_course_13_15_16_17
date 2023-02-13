87eadb1af17d06dacdf930824855585b39f7313c
HaroldSP <george.st.petersburg@gmail.com>
2023-02-14 00:51:29 +0300

    +localstorage workflow

--- js/script.js
+++ js/script.js
@@ -4,22 +4,13 @@
 
 'strict';
 
-// Сохранять данные о делах в localStorage (советую в виде массива)
-// Дела из localStorage подгружаться должны автоматически при загрузки странице
-
-// внимание чтобы сохранить массив в localStorage необходимо его конвертировать в json формат (JSON.stringify)
-
-// внимание из localStorage мы всегда получаем json строку и её необходимо конвертировать обратно в формат javascript (JSON.parse)
-
-//  Проверить, чтобы все работало и не было ошибок в консоли (Учесть вариант отсутствия объекта в localstorage пользователя при первой загрузке страницы)
-
 const todoControl = document.querySelector('.todo-control');
 const headerInput = document.querySelector('.header-input');
 const todoList = document.querySelector('.todo-list');
 const todoCompleted = document.querySelector('.todo-completed');
-// const toDoRemove = document.querySelector('.todo-remove');
 
-const toDoData = [];
+let toDoData = [];
+if (localStorage.toDoData !== undefined) toDoData = JSON.parse(localStorage.getItem('toDoData', toDoData));
 
 const render = function () {
   todoList.innerHTML = '';
@@ -42,14 +33,18 @@ const render = function () {
 
     li.querySelector('.todo-complete').addEventListener('click', function () {
       item.completed = !item.completed;
+      localStorage.clear();
       render();
     })
 
     li.querySelector('.todo-remove').addEventListener('click', function () {
       toDoData.splice(index, 1);
+      localStorage.clear();
       render();
     })
   })
+
+  localStorage.setItem('toDoData', JSON.stringify(toDoData));
 }
 
 todoControl.addEventListener('submit', function (event) {
@@ -65,3 +60,5 @@ todoControl.addEventListener('submit', function (event) {
 
   render();
 })
+
+render();
