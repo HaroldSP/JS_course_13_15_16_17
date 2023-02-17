007ab7e4f88059919132b736c15fde4e7d2286d0
HaroldSP <george.st.petersburg@gmail.com>
2023-02-18 01:02:00 +0300

    lesson17_practice_webclient: objects and basic html (made class constructor, form with required fields

--- js/script.js
+++ js/script.js
@@ -50,3 +50,81 @@ class Driver extends Person {
   set newAddict (value) { this.addict = value };
   set newCar (value) { this.car = value };
 };
+
+// //////////////////////////////////////////////////////////////////////////////////
+const form = document.getElementById('myForm');
+const submitButton = document.getElementById('submit');
+
+const select = document.getElementById('my-select');
+
+let smithDiv = document.querySelector('.extra-form-smith');
+let driverDiv = document.querySelector('.extra-form-driver');
+
+const traumas = document.getElementById('traumas');
+const notraumas = document.getElementById('notraumas');
+const hasinstrument = document.getElementById('hasinstrument');
+const noinstrument = document.getElementById('noinstrument');
+
+const addicted = document.getElementById('addicted');
+const notaddicted = document.getElementById('notaddicted');
+const hascar = document.getElementById('hascar');
+const nocar = document.getElementById('nocar');
+
+const showDiv = function () {
+  const select = document.getElementById('my-select');
+  const selectedOption = select.options[select.selectedIndex].value;
+
+  if (selectedOption === 'smith') {
+    smithDiv.style.display = 'block';
+    traumas.required = true;
+    notraumas.required = true;
+    hasinstrument.required = true;
+    noinstrument.required = true;
+    addicted.required = false;
+    notaddicted.required = false;
+    hascar.required = false;
+    nocar.required = false;
+  } else if (selectedOption === 'driver') {
+    driverDiv.style.display = 'block';
+    traumas.required = false;
+    notraumas.required = false;
+    hasinstrument.required = false;
+    noinstrument.required = false;
+    addicted.required = true;
+    notaddicted.required = true;
+    hascar.required = true;
+    nocar.required = true;
+  }
+};
+
+select.addEventListener('change', showDiv);
+
+const submitForm = function (event) {
+  event.preventDefault();
+
+  let nameField = document.getElementById('name').value;
+  let surnameField = document.getElementById('surname').value;
+  let selectedGender = document.querySelector('input[name="gender"]:checked').nextSibling.textContent.trim();
+  let ageField = document.getElementById('Age').value;
+  let healthField = document.getElementById('');
+  let instrumentField = document.getElementById('');
+  let addictField = document.getElementById('');
+  let carField = document.getElementById('');
+  console.log(nameField, surnameField, selectedGender, ageField);
+
+  form.reset();
+  document.getElementById('my-select').options[select.selectedIndex].value = '';
+  smithDiv.style.display = 'none';
+  driverDiv.style.display = 'none';
+
+  traumas.required = false;
+  notraumas.required = false;
+  hasinstrument.required = false;
+  noinstrument.required = false;
+  addicted.required = false;
+  notaddicted.required = false;
+  hascar.required = false;
+  nocar.required = false;
+}
+
+form.addEventListener('submit', submitForm);
