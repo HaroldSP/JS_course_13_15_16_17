8c8de496af7eea02202e136cfe74c048ef9466cf
HaroldSP <george.st.petersburg@gmail.com>
2023-02-17 18:33:18 +0300

    lesson17_practice_webclient: objects and basic html

--- js/script.js
+++ js/script.js
@@ -4,20 +4,49 @@
 
 'strict';
 
-class First {
-  hello () {
-    console.log('Привет я метод родителя!');
-  }
+class Person {
+  constructor (name, surname, gender, age) {
+    this.name = name;
+    this.surname = surname;
+    this.gender = gender;
+    this.age = age;
+  };
+
+  get newName () { return this.name };
+  get newSurname () { return this.surname };
+  get newGender () { return this.gender };
+  get newAge () { return this.age };
+
+  set newName (value) { this.name = value };
+  set newSurname (value) { this.surname = value };
+  set newGender (value) { this.gender = value };
+  set newAge (value) { this.age = value };
 };
 
-class Second extends First {
-  hello () {
-    super.hello();
-    console.log('А я наследуемый метод!');
+class Smith extends Person {
+  constructor (name, surname, gender, age, health, instrument) {
+    super(name, surname, gender, age);
+    this.health = health;
+    this.instrument = instrument;
   }
+
+  get newHealth () { return this.health };
+  get newInstrument () { return this.instrument };
+
+  set newHealth (value) { this.health = value };
+  set newInstrument (value) { this.instrument = value };
 };
 
-const example = new Second();
+class Driver extends Person {
+  constructor (name, surname, gender, age, addict, car) {
+    super(name, surname, gender, age);
+    this.addict = addict;
+    this.car = car;
+  }
 
-console.log(example);
-example.hello();
+  get newAddict () { return this.addict };
+  get newCar () { return this.car };
+
+  set newAddict (value) { this.addict = value };
+  set newCar (value) { this.car = value };
+};
