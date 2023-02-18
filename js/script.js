2245b3f44a9d493fd38645cd4ba4a59f56f1ddea
HaroldSP <george.st.petersburg@gmail.com>
2023-02-18 21:50:08 +0300

    lesson17_practice_webclient: objects and basic html (before inserting profession)

--- js/script.js
+++ js/script.js
@@ -56,6 +56,7 @@ const form = document.getElementById('myForm');
 const submitButton = document.getElementById('submit');
 
 const select = document.getElementById('my-select');
+let selectedOption = select.options[select.selectedIndex].value;
 
 let smithDiv = document.querySelector('.extra-form-smith');
 let driverDiv = document.querySelector('.extra-form-driver');
@@ -70,9 +71,12 @@ const notaddicted = document.getElementById('notaddicted');
 const hascar = document.getElementById('hascar');
 const nocar = document.getElementById('nocar');
 
+let data = [];
+if (localStorage.data !== undefined) data = JSON.parse(localStorage.getItem('data', data));
+
 const showDiv = function () {
   const select = document.getElementById('my-select');
-  const selectedOption = select.options[select.selectedIndex].value;
+  selectedOption = select.options[select.selectedIndex].value;
 
   if (selectedOption === 'smith') {
     smithDiv.style.display = 'block';
@@ -99,6 +103,34 @@ const showDiv = function () {
 
 select.addEventListener('change', showDiv);
 
+const render = function () {
+  let table = document.getElementById('myTable');
+
+  for (let i = table.rows.length - 1; i > 0; i--) {
+    table.deleteRow(i);
+  }
+
+  data.forEach(function (item, index) {
+    let newRow = table.insertRow();
+    const cols = 9;
+
+    const keys = Object.keys(item);
+    console.dir(keys[0]);
+    for (let i = 0; i < cols; i++) {
+      const cell = newRow.insertCell();
+      cell.innerHTML = item[keys[i]];
+      // console.dir(item);
+    }
+    // li.querySelector('.todo-remove').addEventListener('click', function () {
+    //   data.splice(index, 1);
+    //   localStorage.clear();
+    //   render();
+    // })
+  })
+
+  localStorage.setItem('data', JSON.stringify(data));
+};
+
 const submitForm = function (event) {
   event.preventDefault();
 
@@ -106,11 +138,23 @@ const submitForm = function (event) {
   let surnameField = document.getElementById('surname').value;
   let selectedGender = document.querySelector('input[name="gender"]:checked').nextSibling.textContent.trim();
   let ageField = document.getElementById('Age').value;
-  let healthField = document.getElementById('');
-  let instrumentField = document.getElementById('');
-  let addictField = document.getElementById('');
-  let carField = document.getElementById('');
-  console.log(nameField, surnameField, selectedGender, ageField);
+
+  if (selectedOption === 'smith') {
+    let healthField = document.querySelector('input[name="health"]:checked').nextSibling.textContent.trim();
+    let instrumentField = document.querySelector('input[name="instrument"]:checked').nextSibling.textContent.trim();
+
+    const newSmith = new Smith(nameField, surnameField, selectedGender, ageField, healthField, instrumentField);
+    // console.log(newSmith);
+    data.push(newSmith);
+    //
+  } else if (selectedOption === 'driver') {
+    let addictField = document.querySelector('input[name="addict"]:checked').nextSibling.textContent.trim();
+    let carField = document.querySelector('input[name="car"]:checked').nextSibling.textContent.trim();
+
+    const newDriver = new Driver(nameField, surnameField, selectedGender, ageField, addictField, carField);
+    data.push(newDriver);
+  };
+  // console.log(data);
 
   form.reset();
   document.getElementById('my-select').options[select.selectedIndex].value = '';
@@ -125,6 +169,13 @@ const submitForm = function (event) {
   notaddicted.required = false;
   hascar.required = false;
   nocar.required = false;
+
+  render();
 }
 
 form.addEventListener('submit', submitForm);
+
+function deleteRow (btn) {
+  let row = btn.parentNode.parentNode;
+  row.parentNode.removeChild(row);
+};
