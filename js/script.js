3d8e94dd135ee860284f4f06b6dda771dd47d481
HaroldSP <george.st.petersburg@gmail.com>
2023-02-18 23:14:58 +0300

    lesson17_practice_webclient: objects and basic html (correct order of cells)

--- js/script.js
+++ js/script.js
@@ -24,29 +24,35 @@ class Person {
 };
 
 class Smith extends Person {
-  constructor (name, surname, gender, age, health, instrument) {
+  constructor (name, surname, gender, age, job = 'слесарь', health, instrument) {
     super(name, surname, gender, age);
+    this.job = job;
     this.health = health;
     this.instrument = instrument;
   }
 
+  get newJob () { return this.job };
   get newHealth () { return this.health };
   get newInstrument () { return this.instrument };
 
+  set newJob (value) { this.job = value };
   set newHealth (value) { this.health = value };
   set newInstrument (value) { this.instrument = value };
 };
 
 class Driver extends Person {
-  constructor (name, surname, gender, age, addict, car) {
+  constructor (name, surname, gender, age, job = 'водитель', addict, car) {
     super(name, surname, gender, age);
+    this.job = job;
     this.addict = addict;
     this.car = car;
   }
 
+  get newJob () { return this.job };
   get newAddict () { return this.addict };
   get newCar () { return this.car };
 
+  set newJob (value) { this.job = value };
   set newAddict (value) { this.addict = value };
   set newCar (value) { this.car = value };
 };
@@ -103,6 +109,8 @@ const showDiv = function () {
 
 select.addEventListener('change', showDiv);
 
+// /////////////////////////////////////////////////////////////        render function              ////////////////////////////////////////////
+
 const render = function () {
   let table = document.getElementById('myTable');
 
@@ -112,14 +120,25 @@ const render = function () {
 
   data.forEach(function (item, index) {
     let newRow = table.insertRow();
-    const cols = 9;
 
+    const cols = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
     const keys = Object.keys(item);
-    console.dir(keys[0]);
-    for (let i = 0; i < cols; i++) {
-      const cell = newRow.insertCell();
-      cell.innerHTML = item[keys[i]];
-      // console.dir(item);
+    let j = 0;
+
+    for (let i = 0; i <= 9; i++) {
+      if ((item[keys[4]] === 'Слеcарь') && ((i === 7) || (i === 8))) {
+        newRow.insertCell();
+        j += 0;
+        continue;
+      } else if ((item[keys[4]] === 'Водитель') && ((i === 5) || (i === 6))) {
+        newRow.insertCell();
+        j += 0;
+        continue;
+      } else {
+        const cell = newRow.insertCell();
+        cell.innerHTML = item[keys[j]];
+        j += 1;
+      }
     }
     // li.querySelector('.todo-remove').addEventListener('click', function () {
     //   data.splice(index, 1);
@@ -138,12 +157,13 @@ const submitForm = function (event) {
   let surnameField = document.getElementById('surname').value;
   let selectedGender = document.querySelector('input[name="gender"]:checked').nextSibling.textContent.trim();
   let ageField = document.getElementById('Age').value;
+  let jobField = select.options[select.selectedIndex].innerHTML;
 
   if (selectedOption === 'smith') {
     let healthField = document.querySelector('input[name="health"]:checked').nextSibling.textContent.trim();
     let instrumentField = document.querySelector('input[name="instrument"]:checked').nextSibling.textContent.trim();
 
-    const newSmith = new Smith(nameField, surnameField, selectedGender, ageField, healthField, instrumentField);
+    const newSmith = new Smith(nameField, surnameField, selectedGender, ageField, jobField, healthField, instrumentField);
     // console.log(newSmith);
     data.push(newSmith);
     //
@@ -151,7 +171,7 @@ const submitForm = function (event) {
     let addictField = document.querySelector('input[name="addict"]:checked').nextSibling.textContent.trim();
     let carField = document.querySelector('input[name="car"]:checked').nextSibling.textContent.trim();
 
-    const newDriver = new Driver(nameField, surnameField, selectedGender, ageField, addictField, carField);
+    const newDriver = new Driver(nameField, surnameField, selectedGender, ageField, jobField, addictField, carField);
     data.push(newDriver);
   };
   // console.log(data);
